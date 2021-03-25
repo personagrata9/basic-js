eaa688180a3cbb01e64f6135130cd46cea6c6102
personagrata9 <ira.bakhur@icloud.com>
2021-03-26 01:21:09 +0300

    feat: add solution to dream-team subtask

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if ( !Array.isArray(members) || members.length === 0 || members === null ) return false;
+  let membersSorted = members.map((elem) => typeof elem === 'string' ?  elem.trim() : elem);
+  let team = [];
+  for (let i = 0; i < membersSorted.length; i++) {
+    if ( typeof membersSorted[i] === 'string') {
+    team.push(membersSorted[i].substr(0, 1).toUpperCase());
+  }
+  }
+  let res = team.sort().join('');
+  return res;
 };
