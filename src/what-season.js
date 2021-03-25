0b654a34229303f26bb4bf3eb59782965d434859
personagrata9 <ira.bakhur@icloud.com>
2021-03-26 01:25:00 +0300

    feat: add solution to carbon-dating subtask

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (Boolean(date) == false) return 'Unable to determine the time of year!';
+  try {
+    !date.getTime();
+  } catch(e) {
+    throw new Error(e);
+  }
+  if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) return 'winter';
+  if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) return 'spring';
+  if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) return 'summer';
+  if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) return 'autumn';
 };
