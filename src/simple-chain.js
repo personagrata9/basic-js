0d9e32f51f90db4aa201f94d3ca01d83cf82149e
personagrata9 <ira.bakhur@icloud.com>
2021-03-28 01:06:16 +0300

    feat: add solution to simple-chain subtask

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,31 @@
+const { resetHistory } = require("sinon");
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(`( ${String(value)} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   if ( typeof position !== 'number' || !Number.isInteger(position) || position < 0 || position === 0 || position > this.chain.length) {
+     this.chain.length = 0;
+      throw new Error;
+    }
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.chain.join('~~');
+    this.chain.length = 0; // удаление существующей цепи
+    return res;
   }
 };
 
