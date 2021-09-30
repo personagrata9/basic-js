a8377158e19f7e722dfa8c045ff385c12ef5aeb3
personagrata9 <ira.bakhur@gmail.com>
2021-09-30 23:48:07 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,74 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(mode = true) {
+		this.mode = mode;
+	}
+
+  encrypt(message, key) {
+    try {
+      !message || !key;
+    } catch(e) {
+      throw new Error(e);
+    }
+
+    let alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    let messageLtrs = message.toUpperCase();
+    let keyLtrs = [];
+    let k = 0;
+    for (let m = 0; m < message.length; m++) {
+      if ( alph.includes(messageLtrs[m]) ) {
+        keyLtrs.push(key.toUpperCase()[k % key.length]);
+        k++;
+      } else {
+        keyLtrs.push(messageLtrs[m]);
+      }
+    }
+
+    let res = [];
+    for (let r = 0; r < message.length; r++) {
+      let messageLtrInd = alph.findIndex((elem) => elem == messageLtrs[r]);
+      let keyLtrInd = alph.findIndex((elem) => elem == keyLtrs[r]);
+      if ( alph.includes(messageLtrs[r]) ) {
+        res.push( alph[ (messageLtrInd + keyLtrInd) % 26 ] );
+      } else {
+        res.push(messageLtrs[r]);
+      }
+    }
+    return this.mode === true ? res.join('') : res.reverse().join('');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   
+  decrypt(message, key) {
+    try {
+      !message || !key;
+    } catch(e) {
+      throw new Error(e);
+    }
+
+    let alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    let messageLtrs = message.toUpperCase();
+    let keyLtrs = [];
+    let k = 0;
+    for (let m = 0; m < message.length; m++) {
+      if ( alph.includes(messageLtrs[m]) ) {
+        keyLtrs.push(key.toUpperCase()[k % key.length]);
+        k++;
+      } else {
+        keyLtrs.push(messageLtrs[m]);
+      }
+    }
+
+    let res = [];
+    for (let r = 0; r < message.length; r++) {
+      let messageLtrInd = alph.findIndex((elem) => elem == messageLtrs[r]);
+      let keyLtrInd = alph.findIndex((elem) => elem == keyLtrs[r]);
+      if ( alph.includes(messageLtrs[r]) ) {
+        res.push( alph[ (messageLtrInd - keyLtrInd + 26) % 26 ] );
+      } else {
+        res.push(messageLtrs[r]);
+      }
+    }
+    return this.mode === true ? res.join('') : res.reverse().join('');
   }
 }
